9b4d49aafb6d3f62856f24e630cdd66c38e0519f
bridgeyuwa <torkumajonathanyuwa@gmail.com>
2022-03-29 01:25:58 +0100

    Revert "update"
    This reverts commit 487bff1dec1ea743201dd4c935da3f4d9c3b0c8c.

--- config/auth.php
+++ config/auth.php
@@ -24,14 +24,14 @@ return [
     |--------------------------------------------------------------------------
     |
     | Next, you may define every authentication guard for your application.
-    | Of program, a great default configuration has been defined for you
+    | Of course, a great default configuration has been defined for you
     | here which uses session storage and the Eloquent user provider.
     |
     | All authentication drivers have a user provider. This defines how the
     | users are actually retrieved out of your database or other storage
     | mechanisms used by this application to persist your user's data.
     |
-    | Supported: "session"
+    | Supported: "session", "token"
     |
     */
 
@@ -39,6 +39,12 @@ return [
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
+        ],
+
+        'api' => [
+            'driver' => 'token',
+            'provider' => 'users',
+            'hash' => false,
         ],
     ],
 
@@ -62,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\User::class,
         ],
 
         // 'users' => [
